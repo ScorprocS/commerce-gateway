c8b46cd8d91c132feb5cbb6504d0d94e5ae1864a
Alex <a.blanc100@laposte.net>
2023-11-28 15:20:58 +0100

    first commit

---
+++ src/main/java/lu/sfeir/commerce/gateway/CommerceGatewayApplication.java
@@ -0,0 +1,13 @@
+package lu.sfeir.commerce.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lu/sfeir/commerce/gateway/CommerceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.sfeir.commerce.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

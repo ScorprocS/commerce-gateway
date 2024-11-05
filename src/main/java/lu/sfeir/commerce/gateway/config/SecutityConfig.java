8178b0143e24c447449272fee8cd37482a463063
Alex <a.blanc100@laposte.net>
2024-11-05 09:43:13 +0100

    security keycloak setup

--- src/main/java/lu/sfeir/commerce/gateway/config/SecutityConfig.java
+++ src/main/java/lu/sfeir/commerce/gateway/config/SecutityConfig.java
@@ -1,15 +1,21 @@
 package lu.sfeir.commerce.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecutityConfig {
+	@Value("${keycloak.issuer}")
+    private String issuerUri;
+
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
 		return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
@@ -18,6 +24,11 @@ public class SecutityConfig {
 						.anyExchange().authenticated())
 				.oauth2ResourceServer((oauth)-> oauth.jwt(Customizer.withDefaults()))
 						.build();
+	}
+	
+	@Bean
+	public ReactiveJwtDecoder jwtDecoder() {
+		return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
 	}
 
 }
